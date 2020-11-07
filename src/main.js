67c060685b9b16477c14c7b3c432cf124abce7e1
Arpan Adhikari <arpan@MacBook-Air.local>
2020-11-08 00:13:51 +0530

    Fixed not rendering issue due to axios

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
-import axios from 'axios'
 
 
-createApp(App).use(router).use(store).use(axios).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
